2d306a07725d41ce7b75f682fb3693855f415817
HiMatterhorn <wyp4tomasz@gmail.com>
2021-06-30 20:14:04 +0200

    kot-domain branch checked

--- AppCats/koty-domain/src/main/java/pl/kobietydokodu/koty/domain/Kot.java
+++ AppCats/koty-domain/src/main/java/pl/kobietydokodu/koty/domain/Kot.java
@@ -3,41 +3,50 @@ package pl.kobietydokodu.koty.domain;
 import java.util.Date;
 
 public class Kot {
-	
+
+
 	private String imie;
-    private Date dataUrodzenia;
-    private Float waga;
-    private String imieOpiekuna;
-    
+	private Date dataUrodzenia;
+	private Float waga;
+	private String imieOpiekuna;
 
 	public String getImie() {
 		return imie;
 	}
+
 	public void setImie(String imie) {
 		this.imie = imie;
 	}
+
 	public Date getDataUrodzenia() {
 		return dataUrodzenia;
 	}
+
 	public void setDataUrodzenia(Date dataUrodzenia) {
 		this.dataUrodzenia = dataUrodzenia;
 	}
+
 	public Float getWaga() {
 		return waga;
 	}
+
 	public void setWaga(Float waga) {
 		this.waga = waga;
 	}
+
 	public String getImieOpiekuna() {
 		return imieOpiekuna;
 	}
+
 	public void setImieOpiekuna(String imieOpiekuna) {
 		this.imieOpiekuna = imieOpiekuna;
 	}
 
-    @Override
+	@Override
 	public String toString() {
 		return "Kot [imie=" + imie + ", dataUrodzenia=" + dataUrodzenia + ", waga=" + waga + ", imieOpiekuna="
 				+ imieOpiekuna + "]";
 	}
+
+
 }
